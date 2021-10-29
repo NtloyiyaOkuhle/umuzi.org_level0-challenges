e72c5cbab693809407ef1cd9c7a553b9804cf0c7
Okuhle <blessing.ntloyiya@gmail.com>
2021-10-29 14:02:16 +0200

    Add files via upload

--- task0.8.js
+++ task0.8.js
@@ -1,7 +1,11 @@
-function timeConverter(num){
-    let hours = Math.floor(num/60);
-    let minutes = num%60;
-    console.log(hours + " hour" + "," + minutes + " minute");
-}
 
-console.log(timeConverter(71));
+function timeConverter(n) {
+    let num = n;
+    let hours = (num / 60);
+    let rhours = Math.floor(hours);
+    let minutes = (hours - rhours) * 60;
+    let rminutes = Math.round(minutes);
+    console.log( rhours + " hour(s) : " + rminutes + " minute(s).");
+    }
+    
+    console.log(timeConverter(61));
